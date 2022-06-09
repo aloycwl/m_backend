7fd771bbb4807e0eb2d3899f2bdb3e881527d2b2
"Aloysius" <"aloycwl@gmail.com">
2022-06-09 16:36:18 +0800

    General function v1

--- main.sol
+++ main.sol
@@ -1,4 +1,20 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
+interface IERC721{
+    function owner()external view returns(address);
+}
 contract Main{
-    
+    address[]public clubs;
+    function addClubs(address a)external{
+        require(IERC721(a).owner()==msg.sender);
+        clubs.push(a);
+    }
+    function removeClubs(address a)external{
+        require(IERC721(a).owner()==msg.sender);
+        for(uint i=0;i<clubs.length;i++)
+        if(clubs[i]==a){
+            clubs[i]=clubs[clubs.length-1];
+            clubs.pop();
+            return;
+        }
+    }
 }
