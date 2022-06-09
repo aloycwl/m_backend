22d777826b734fdf1d1f6961a0b866f8e8477358
"Aloysius" <"aloycwl@gmail.com">
2022-06-09 17:01:16 +0800

    getClubs getOwned

--- main.sol
+++ main.sol
@@ -1,6 +1,7 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
 interface IERC721{
     function owner()external view returns(address);
+    function balanceOf(address)external view returns(uint);
 }
 contract Main{
     address[]public clubs;
@@ -15,6 +16,23 @@ contract Main{
             clubs[i]=clubs[clubs.length-1];
             clubs.pop();
             return;
+        }
+    }
+    function getClubs()external view returns(address[]memory){
+        return clubs;
+    }
+    function getOwned()view external returns(address[]memory a,uint[]memory b){
+        uint count;
+        for(uint i=0;i<clubs.length;i++)
+        if(IERC721(clubs[i]).balanceOf(msg.sender)>0)count++;
+        (a,b)=(new address[](count),new uint[](count));
+        count=0;
+        for(uint i=0;i<clubs.length;i++){
+            uint bal=IERC721(clubs[i]).balanceOf(msg.sender);
+            if(bal>0){
+                a[0]=clubs[i];
+                b[0]=bal;
+            }
         }
     }
 }
